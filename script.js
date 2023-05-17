24c7eee154081d54dbc180b91e0b0494245713e5
WidyaX1900 <tuderangga93@gmail.com>
2023-05-17 20:16:13 +0800

    Update file

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const hamburger = document.getElementById('hamburger');
 const hamburgerIcon = hamburger.getElementsByTagName('div');
+const menu = document.getElementsByClassName('menu-list')[0];
 
 hamburger.addEventListener("click", event => {
    for (let i = 0; i < hamburgerIcon.length; i++) {
     
         hamburgerIcon[i].classList.toggle('icon-' + (i + 1));
-   }       
+   }
+   
+   menu.classList.toggle('slider');
 });
